242bfee03b3e2e673699a100d9456d7547dc8b74
sonal.k <sonal.k@SonalK-PC.afourtech.pune>
2019-02-05 18:02:30 +0530

    first commit

---
+++ GitHub Practice Project/src/com/github/A.java
@@ -0,0 +1,8 @@
+package com.github;
+
+public class A {
+public static void main(String[] args) {
+	System.out.println("You are in A");
+
+}
+}
---
+++ GitHub Practice Project/src/com/github/B.java
@@ -0,0 +1,8 @@
+package com.github;
+
+public class B {
+public static void main(String[] args) {
+	
+	System.out.println("You are in B");
+}
+}
